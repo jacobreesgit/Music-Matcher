3a7077d7600b0ecceb71e503df6164091c426421
jacobreesgit <jacobrees@me.com>
2025-06-04 16:42:58 +0100

    Initial Commit

---
+++ MusicRepeater/MusicRepeaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicRepeaterApp.swift
+//  MusicRepeater
+//
+//  Created by Jacob Rees on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicRepeaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
